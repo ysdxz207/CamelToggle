4002e371f0f48a40598552754080d1c49ad5494d
ysdxz207 <ysdxz207@qq.com>
2017-07-28 15:32:15 +0800

    :smile:

---
+++ src/com/puyixiaowo/cameltoggle/actions/ToggleCamelAction.java
@@ -0,0 +1,46 @@
+package com.puyixiaowo.cameltoggle.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.*;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.util.TextRange;
+import com.puyixiaowo.cameltoggle.utils.CamelCaseUtils;
+
+/**
+ * @author Moses
+ * @date 2017-07-28 14:07
+ */
+public class ToggleCamelAction extends AnAction {
+    @Override
+    public void actionPerformed(AnActionEvent anActionEvent) {
+        //Get all the required data from data keys
+        final Editor editor = anActionEvent.getRequiredData(CommonDataKeys.EDITOR);
+        final Project project = anActionEvent.getRequiredData(CommonDataKeys.PROJECT);
+        //Access document, caret, and selection
+        final Document document = editor.getDocument();
+        final SelectionModel selectionModel = editor.getSelectionModel();
+
+        final int start = selectionModel.getSelectionStart();
+        final int end = selectionModel.getSelectionEnd();
+
+        String text = document.getText(new TextRange(start, end));
+
+        String replacement = CamelCaseUtils.checkIsCamelCase(text) ? CamelCaseUtils.toUnderlineName(text) :
+                CamelCaseUtils.toCamelCase(text);
+        //New instance of Runnable to make a replacement
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                document.replaceString(start, end, replacement);
+            }
+        };
+        //Making the replacement
+        WriteCommandAction.runWriteCommandAction(project, runnable);
+        selectionModel.removeSelection();
+    }
+}
